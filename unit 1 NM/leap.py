0eea373c5f6c0a592962f60b671c411d3036226b
jophilla <144616047+jophilla@users.noreply.github.com>
2023-09-11 07:38:49 +0530

    Add files via upload

---
+++ unit 1 NM/leap.py
@@ -0,0 +1,14 @@
+# Input from the user
+try:
+    year = int(input("Enter a year: "))
+
+    # Check if it's a leap year
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print(f"{year} is a leap year.")
+    else:
+        print(f"{year} is not a leap year.")
+except ValueError:
+    print("Invalid input. Please enter a valid year (e.g., 2023).")
+
+
+
